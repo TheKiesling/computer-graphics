c85247b69c796361566daead785e8ad70124e8f3
José Kiesling <kie21581@uvg.edu.gt>
2023-07-23 21:53:39 -0600

    Lab 1

--- labs/lab1/src/bitmap.h
+++ labs/lab1/src/bitmap.h
@@ -5,18 +5,33 @@
 struct Bitmap{
     Framebuffer framebuffer = Framebuffer();
     const std::string filename = "out.bmp";
+    std::vector<Vertex2> polygon1 = {Vertex2(165, 380), Vertex2(185, 360), Vertex2(180, 330), 
+                                     Vertex2(207, 345), Vertex2(233, 330), Vertex2(230, 360),
+                                     Vertex2(250, 380), Vertex2(220, 385), Vertex2(205, 410),
+                                     Vertex2(193, 383)};
+
+    std::vector<Vertex2> polygon2 = {Vertex2(321, 335), Vertex2(288, 286), Vertex2(339, 251), 
+                                     Vertex2(374, 302)};
+    
+    std::vector<Vertex2> polygon3 = {Vertex2(377, 249), Vertex2(411, 197), Vertex2(436, 249)};
+
+    std::vector<Vertex2> polygon4 = {Vertex2(413, 177), Vertex2(448, 159), Vertex2(502, 88), 
+                                     Vertex2(553, 53), Vertex2(535, 36), Vertex2(676, 37), 
+                                     Vertex2(660, 52), Vertex2(750, 145), Vertex2(761, 179), 
+                                     Vertex2(672, 192), Vertex2(659, 214), Vertex2(615, 214), 
+                                     Vertex2(632, 230), Vertex2(580, 230), Vertex2(597, 215), 
+                                     Vertex2(552, 214), Vertex2(517, 144), Vertex2(466, 180)};
+                                    
+    std::vector<Vertex2> polygon5 = {Vertex2(682, 175), Vertex2(708, 120), Vertex2(735, 148),
+                                     Vertex2(739, 170)};
 
     void render() {
         framebuffer.setClearColor(Color(0, 0, 0));
         framebuffer.clear();
-
-        framebuffer.setCurrentColor(Color(100, 150, 100));
-        framebuffer.point(Vertex2(100, 100));
-        framebuffer.point(Vertex2(200, 200));
-        framebuffer.drawLine(100, 100, 200, 200);
-        std::vector<Vertex2> points = {Vertex2(100, 100), Vertex2(200, 200), Vertex2(150, 300)};
-        framebuffer.drawPolygon(points);
-        framebuffer.fillPolygon(points);
+        drawPolygon1();
+        drawPolygon2();
+        drawPolygon3();
+        drawPolygon4();
         renderBuffer();
     }
 
@@ -76,6 +91,37 @@ struct Bitmap{
         file.close();
 
         std::cout << "Archivo " << filename << " generado correctamente." << std::endl;
+    }
+
+    void drawPolygon1(){
+        framebuffer.setCurrentColor(Color(255, 255, 255));
+        framebuffer.drawPolygon(polygon1);
+        framebuffer.setCurrentColor(Color(255, 255, 0));
+        framebuffer.fillPolygon(polygon1);
+    }
+
+    void drawPolygon2(){
+        framebuffer.setCurrentColor(Color(255, 255, 255));
+        framebuffer.drawPolygon(polygon2);
+        framebuffer.setCurrentColor(Color(0, 0, 255));
+        framebuffer.fillPolygon(polygon2);
+    }
+
+    void drawPolygon3(){
+        framebuffer.setCurrentColor(Color(255, 255, 255));
+        framebuffer.drawPolygon(polygon3);
+        framebuffer.setCurrentColor(Color(255, 0, 0));
+        framebuffer.fillPolygon(polygon3);
+    }
+
+    void drawPolygon4(){
+        framebuffer.setCurrentColor(Color(255, 255, 255));
+        framebuffer.drawPolygon(polygon4);
+        framebuffer.setCurrentColor(Color(0, 255, 0));
+        framebuffer.fillPolygon(polygon4);
+        framebuffer.setCurrentColor(Color(0, 0, 0));
+        framebuffer.drawPolygon(polygon5);
+        framebuffer.fillPolygon(polygon5);
     }
 
 };
